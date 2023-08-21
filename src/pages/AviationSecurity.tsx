2b1e1f0d9a79e9e3c70ddfccc4d62cab6a135097
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-08-21 13:11:08 +0300

    Temp commit

--- src/pages/AviationSecurity.tsx
+++ src/pages/AviationSecurity.tsx
@@ -41,45 +41,65 @@ function AviationSecurity() {
         setSelectedAnswers(updatedAnswers);
     };
 
+    const handleBackButtonClick = () => {
+        if (currentQuestionIndex > 0) {
+            setCurrentQuestionIndex(currentQuestionIndex - 1);
+            setSelectedAnswers([]);
+        }
+    };
+
     return (
         <div className="wrapper">
-            <h1 className='title'>Авіаційна безпека</h1>
-            {showScore ? (
-                <div className="score-section">
-                    Ви вірно відповіли на {score} питань з {questions.length} Ваш бал - {score*100/questions.length}
-                </div>
-            ) : (
-                <div className="question-section">
-                    <div className="question-count">
-                        <span>Питання {currentQuestionIndex + 1}</span>/
-                        {questions.length}
+            <div className="bg_wrapper">
+                <h1 className="title">Авіаційна безпека</h1>
+                {showScore ? (
+                    <div className="score-section">
+                        Ви вірно відповіли на {score} питань з{' '}
+                        {questions.length} Ваш бал -{' '}
+                        {((score * 100) / questions.length).toFixed(1)}
                     </div>
-                    <div className="question-text">
-                        {questions[currentQuestionIndex].question}
+                ) : (
+                    <div className="question-section">
+                        <div className="question-count">
+                            <span>Питання {currentQuestionIndex + 1}</span>/
+                            {questions.length}
+                        </div>
+                        <div className="question-text">
+                            {questions[currentQuestionIndex].question}
+                        </div>
+                        <div className="answer-options">
+                            {questions[currentQuestionIndex].options.map(
+                                (option, index) => (
+                                    <label key={index}>
+                                        <input
+                                            type="checkbox"
+                                            name="answer"
+                                            value={option}
+                                            checked={selectedAnswers.includes(
+                                                option
+                                            )}
+                                            onChange={() =>
+                                                handleAnswerCheckboxChange(
+                                                    option
+                                                )
+                                            }
+                                        />
+                                        {option}
+                                    </label>
+                                )
+                            )}
+                        </div>
+                        <div className="button-container">
+                            <button onClick={handleBackButtonClick}>
+                                Назад
+                            </button>
+                            <button onClick={handleAnswerButtonClick}>
+                                Далі
+                            </button>
+                        </div>
                     </div>
-                    <div className="answer-options">
-                        {questions[currentQuestionIndex].options.map(
-                            (option, index) => (
-                                <label key={index}>
-                                    <input
-                                        type="checkbox"
-                                        name="answer"
-                                        value={option}
-                                        checked={selectedAnswers.includes(
-                                            option
-                                        )}
-                                        onChange={() =>
-                                            handleAnswerCheckboxChange(option)
-                                        }
-                                    />
-                                    {option}
-                                </label>
-                            )
-                        )}
-                    </div>
-                    <button onClick={handleAnswerButtonClick}>Next</button>
-                </div>
-            )}
+                )}
+            </div>
         </div>
     );
 }
